c6955c38f6959ae85235dbb29dc4fa29f509ca67
DALI <sika.dali@telecomnancy.net>
2024-07-01 01:29:15 +0200

    custom splash screen - icon is now growing

--- lib/src/features/authentication/screens/splash_screen/splash_screen.dart
+++ lib/src/features/authentication/screens/splash_screen/splash_screen.dart
@@ -12,6 +12,7 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   double _opacity = 0.0;
+  double _splashIconSize = 0.0;
 
   @override
   void initState() {
@@ -20,6 +21,7 @@ class _SplashScreenState extends State<SplashScreen> {
     Future.delayed(const Duration(milliseconds: 500), () {
       setState(() {
         _opacity = 1.0;
+        _splashIconSize = MediaQuery.of(context).size.height * 0.2;
       });
       // Wait for the logo animation to complete before going to the next page
       Future.delayed(const Duration(milliseconds: 3000), () {
@@ -31,18 +33,25 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     MediaQueryData mediaQuery = MediaQuery.of(context);
-    double splashIconSize = mediaQuery.size.height * 0.2;
     final bool isDarkMode = mediaQuery.platformBrightness == Brightness.dark;
 
     return Scaffold(
       backgroundColor: isDarkMode ? sSplashDarkColor : sSplashColor,
       body: Center(
-          child: AnimatedOpacity(
-              opacity: _opacity,
-              duration: const Duration(milliseconds: 2000),
-              child: isDarkMode
-                  ? Image(image: const AssetImage(sSplashWhiteIcon), height: splashIconSize)
-                  : Image(image: const AssetImage(sSplashBlackIcon), height: splashIconSize))),
+          child: AnimatedContainer(
+        duration: const Duration(milliseconds: 2000),
+        curve: Curves.easeInOut,
+        // Optional: Add a curve for smoother animation
+        height: _splashIconSize,
+        // Variable to control the height of the image
+        width: _splashIconSize,
+        child: AnimatedOpacity(
+            opacity: _opacity,
+            duration: const Duration(milliseconds: 2000),
+            child: isDarkMode
+                ? const Image(image: AssetImage(sSplashWhiteIcon))
+                : const Image(image: AssetImage(sSplashBlackIcon))),
+      )),
     );
   }
 }
