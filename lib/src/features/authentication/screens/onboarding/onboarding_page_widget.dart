28219e66e18218253904c33aab1559b47c5dec5a
DALI <sika.dali@telecomnancy.net>
2024-07-04 22:28:02 +0200

    content on 3 onBoarding pages

---
+++ lib/src/features/authentication/screens/onboarding/onboarding_page_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:stack_my_money_app/src/constants/sizes.dart';
+import 'package:stack_my_money_app/src/features/authentication/models/onboarding_model.dart';
+
+class OnBoardingPageWidget extends StatelessWidget {
+  const OnBoardingPageWidget({
+    super.key,
+    required this.model,
+  });
+
+  final OnBoardingModel model;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(sDefaultSize),
+      color: model.bgColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image(image: AssetImage(model.image), height: model.screenHeight * 0.25),
+          Column(
+            children: [
+              Text(model.title, style: Theme.of(context).textTheme.displayMedium, textAlign: TextAlign.center),
+              SizedBox(height: 10.0),
+              Text(model.subTitle, style: Theme.of(context).textTheme.titleSmall, textAlign: TextAlign.center),
+            ],
+          ),
+          Text(model.counterText, style: Theme.of(context).textTheme.bodyLarge),
+          SizedBox(height: 50.0)
+        ],
+      ),
+    );
+  }
+}
--- lib/src/features/authentication/screens/onboarding/onboarding_screen.dart
+++ lib/src/features/authentication/screens/onboarding/onboarding_screen.dart
@@ -2,40 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
 import 'package:stack_my_money_app/src/constants/colors.dart';
 import 'package:stack_my_money_app/src/constants/image_strings.dart';
-import 'package:stack_my_money_app/src/constants/sizes.dart';
 import 'package:stack_my_money_app/src/constants/text_strings.dart';
+import 'package:stack_my_money_app/src/features/authentication/models/onboarding_model.dart';
+import 'package:stack_my_money_app/src/features/authentication/screens/onboarding/onboarding_page_widget.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    double imgHeight = MediaQuery.of(context).size.height * 0.25;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    final pages = [
+      OnBoardingPageWidget(
+          model: OnBoardingModel(
+              title: sOnBoardingTitle1,
+              subTitle: sOnBoardingSubTitle1,
+              image: sOnBoardingImage1,
+              counterText: sOnBoardingCounter1,
+              screenHeight: screenHeight,
+              bgColor: sOnBoardingPage1Color)),
+      OnBoardingPageWidget(
+          model: OnBoardingModel(
+              title: sOnBoardingTitle2,
+              subTitle: sOnBoardingSubTitle2,
+              image: sOnBoardingImage2,
+              counterText: sOnBoardingCounter2,
+              screenHeight: screenHeight,
+              bgColor: sOnBoardingPage2Color)),
+      OnBoardingPageWidget(
+          model: OnBoardingModel(
+              title: sOnBoardingTitle3,
+              subTitle: sOnBoardingSubTitle3,
+              image: sOnBoardingImage3,
+              counterText: sOnBoardingCounter3,
+              screenHeight: screenHeight,
+              bgColor: sOnBoardingPage3Color))
+    ];
     return Stack(
       children: [
-        LiquidSwipe(pages: [
-          Container(
-            padding: const EdgeInsets.all(sDefaultSize),
-            color: sOnBoardingPage1Color,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Image(image: AssetImage(sOnBoardingImage1), height: imgHeight,),
-                Column(
-                  children: [
-                    Text(sOnBoardingTitle1, style: Theme.of(context).textTheme.displayMedium, textAlign: TextAlign.center),
-                    SizedBox(height: 10.0),
-                    Text(sOnBoardingSubTitle1, style: Theme.of(context).textTheme.titleSmall, textAlign: TextAlign.center),
-                  ],
-                ),
-                Text(sOnBoardingCounter1, style: Theme.of(context).textTheme.bodyLarge),
-                SizedBox(height: 50.0)
-              ],
-            ),
-          ),
-          Container(color: sOnBoardingPage2Color),
-          Container(color: sOnBoardingPage3Color),
-        ])
+        LiquidSwipe(pages: pages)
       ],
     );
   }
